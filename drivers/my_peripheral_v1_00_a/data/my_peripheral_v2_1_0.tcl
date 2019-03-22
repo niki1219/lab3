b4f3cb620b8257bab7f7989533f4a7a9b4e7382b
Tamara <srecnici97@gmail.com>
2019-03-22 10:26:31 +0100

    Drugi dan, dobar dan :)

---
+++ drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
@@ -0,0 +1,11 @@
+##############################################################################
+## Filename:          C:\Users\student\Desktop\lab3/drivers/my_peripheral_v1_00_a/data/my_peripheral_v2_1_0.tcl
+## Description:       Microprocess Driver Command (tcl)
+## Date:              Fri Mar 22 09:20:24 2019 (by Create and Import Peripheral Wizard)
+##############################################################################
+
+#uses "xillib.tcl"
+
+proc generate {drv_handle} {
+  xdefine_include_file $drv_handle "xparameters.h" "my_peripheral" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" 
+}
---
+++ drivers/my_peripheral_v1_00_a/src/my_peripheral.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          C:\Users\student\Desktop\lab3/drivers/my_peripheral_v1_00_a/src/my_peripheral.c
+* Version:           1.00.a
+* Description:       my_peripheral Driver Source File
+* Date:              Fri Mar 22 09:20:24 2019 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "my_peripheral.h"
+
+/************************** Function Definitions ***************************/
+
